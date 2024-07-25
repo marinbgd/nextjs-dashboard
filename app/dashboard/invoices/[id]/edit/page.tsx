166c2d4919c6aecffea87c87a86d2af984da6985
vmarinovic <vmarinovic@zillowgroup.com>
2024-07-25 12:03:53 +0200

    Adds - server side validation on edit and create invoices

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -6,7 +6,6 @@ import { notFound } from 'next/navigation';
 
 export default async function Page({params}: { params: {id: string}}) {
     const id = params.id;
-    console.log(id)
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
